af824dbff525297da7c73e6cc1cadfa5a153c388
Linna Jansson <linnajansson@Linnas-MacBook-Air.local>
2023-02-21 10:24:48 +0100

    blä

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-purple-500">
       <header className="bg-blue-500">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
